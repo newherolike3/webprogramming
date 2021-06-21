88d7105f0c43b378c935a63f5d8debed1bbca187
Natdanai Tinnakon <newsnail081@gmail.com>
2021-06-21 10:20:42 +0700

    ตัวแปรแบบต่างๆ

--- main.js
+++ main.js
@@ -1,7 +1,21 @@
-let show = "RMUTL"
-let name = "Natdanai Tinnakon"
-
-console.log("Javascript")
-console.log(show)
-console.log(name)
-//document.getElementById("show").innerHTML = show;    
+// Natdanai Tinnakon
+/*
+week 2 - javascript
+*/
+let name = "Natdanai";
+let lastname;//undefined
+lastname = "Tinnakon";
+//console.log(`สวัสดีคุณ ${name} ${lastname}`);
+const PI = 3.14;
+let obj = {
+    name: "Natdanai",
+    lastname: "Tinnakon",
+    address: { 
+     Number:"99/8",
+     province: "พิษณุโลก"
+    }
+};
+console.log(onj.name)
+console.log(onj.lastname);
+console.log(onj.address.Number);
+console.log(onj.address.province);
