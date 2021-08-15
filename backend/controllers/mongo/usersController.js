d2f5d78a5568a568d225b7b0589c3cc6e8f67a65
natdanai tinnakon <newsnail081@gmail.com>
2021-08-16 01:13:26 +0700

    Backend Week 5

--- backend/controllers/mongo/usersController.js
+++ backend/controllers/mongo/usersController.js
@@ -7,3 +7,19 @@ exports.index = async (req, res, next) => {
     res.status(200).json(data);
  
 };
+
+exports.insert = async (req, res, next) => {
+
+let data  = new users({
+    username: req.body.username,
+    email: req.body.email
+});
+
+data .save();
+
+res.status(200).json({
+message: "บันทึกข้อมูลเรียบร้อยแล้ว"
+
+})
+
+};
