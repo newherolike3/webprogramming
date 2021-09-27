161e9408e5c71f81d2e10f459417ac00a57a360f
Natdanai Tinnakon <newsnail081@gmail.com>
2021-09-27 12:37:14 +0700

    login home

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,19 +1,32 @@
 <template>
   <div class="container-fluid">
-<h1> Webprogramming</h1>
-<h2> ณัฐดนัย ทินกร </h2>
-<button type="button" class="btn btn-warning"> Home </button>
-<button type="button" class="btn btn-danger">Danger</button>
-
+      <h1>Webprogramming</h1>
+      <h2>สวัสดีคุณ {{ username }} ({{ email }})</h2>
+      
+      <button @click="logout" type="button" class="btn btn-danger">Logout</button>
   </div>
 </template>
 
 <script>
 export default {
-
+  mounted() {
+    this.email = localStorage.getItem("email");
+    this.username = localStorage.getItem("username");
+  },
+  data() {
+    return {
+      email: null,
+      username: null
+    }
+  },
+  methods: {
+    logout(){
+      localStorage.clear();
+      this.$router.push({ name: "Login" });
+    }
+  }
 }
 </script>
 
 <style>
-
 </style>
